2b5bd1055435496e0ea63657d4b568fa46bcf6ab
taekyeon <wjdxordus@naver.com>
2026-04-17 09:05:52 +0900

    update source

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,13 @@ import TimeBridgeView from '../views/TimeBridgeView.vue'
 import CinemaView from '../views/CinemaView.vue'
 import MyView from '../views/MyView.vue'
 import ProcessingDemoView from '../views/ProcessingDemoView.vue'
+import LetterView from '../views/LetterView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/process' },
+  { path: '/', redirect: '/letter' },
+  { path: '/letter', component: LetterView, meta: { hideGNB: true } },
   { path: '/process', component: ProcessingDemoView, meta: { hideGNB: true } },
   { path: '/home', component: HomeView },
   { path: '/timebridge', component: TimeBridgeView },
